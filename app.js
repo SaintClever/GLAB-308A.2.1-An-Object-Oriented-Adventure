1d39e377371975637aa7cdabb84e332454389779
Saint. Clever <parchment@gmx.com>
2024-10-24 16:29:09 -0400

    Part 4: Class Uniforms

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ for (let i in adventurer.inventory) {
   console.log(adventurer.inventory[i]);
 }
 
-// Give Robin a companion to travel with – a furry friend they call “Leo.”
+// Give Robin a companion to travel with – a furry friend they call "Leo.”
 adventurer.companion = {name: "Leo", type: "Cat"}
 console.log(adventurer);
 
 
-// Add a “companion” sub-object to “Leo” with the following properties:
-// The companion’s name is “Frank.”
-// The companion’s type is “Flea.”
+// Add a "companion” sub-object to "Leo” with the following properties:
+// The companion’s name is "Frank.”
+// The companion’s type is "Flea.”
 // The companion has its own belongings, which includes a small hat and sunglasses.
 adventurer.companion.subCompanion = {
   name: "Frank", type: "Flea",
@@ -37,11 +37,12 @@ console.log(adventurer.roll());
 // --- Part 2: Class Fantasy ---
 class Character {
   constructor (name) {
+    const MAX_HEALTH = 100;
     this.name = name;
     this.health = 100;
     this.inventory = [];
   }
-
+  
   roll = (mod = 0) => {
     const result = Math.floor(Math.random() * 20) + 1 + mod;
     console.log(`${this.name} rolled a ${result}.`)
@@ -63,9 +64,17 @@ console.log(robin.companion.companion.roll);
 // --- Part 3: Class Features ---
 class Adventurer extends Character {
   constructor (name, role) {
+    const ROLES = ["Fighter", "Healer", "Wizar"];
     super(name);
     // Adventurers have specialized roles.
     this.role = role;
+    for (let i in ROLES) {
+      if (ROLES[i] === this.role) {
+        console.log(`${this.role} selected`);
+      } else {
+        console.log(`${this.role} isn't available`);
+      }
+    }
     // Every adventurer starts with a bed and 50 gold coins.
     this.inventory.push("bedroll", "50 gold coins");
   }
